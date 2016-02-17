562679952d357396177f803899e2d78745013f7a
John Robinson <john.robinson@wellzesta.com>
2016-02-17 12:25:18 -0500

    modified the admin pannel to include search and allow filtering by name and date reviewed.

--- books/admin.py
+++ books/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from .models import Book, Author
 
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Book Details", {"fields": ["title", "author"]}),
+        ("Review", {"fields": ["is_favorite", "review", "date_reviewed"]})
+    ]
+
+    def book_authors(self, obj):
+        return obj.list_authors()
+
+    book_authors.short_description = "Author(s)"
+    readonly_fields = ["date_reviewed",]
+    list_display = ["title", "book_authors", "date_reviewed", "is_favorite",]
+    list_editable = ["is_favorite",]
+    list_display_links = ["title","date_reviewed",]
+    search_fields = ["title","author__name",]
+
 # Register your models here.
 admin.site.register(Author)
-admin.site.register(Book)
+
--- books/models.py
+++ books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.timezone import now  
 
 class Book(models.Model):
     title = models.CharField(max_length=150)
@@ -8,7 +9,17 @@ class Book(models.Model):
     is_favorite = models.BooleanField(default=False, verbose_name="Favorite?")
 
     def __str__(self):
-        return self.title
+        return "{} by {}".format(self.title, self.list_authors())
+
+    def list_authors(self):
+        return ", ".join([author.name for author in self.author.all()])
+
+    def save(self, *args, **kwargs):
+        if (self.review and self.date_reviewed is None):
+            self.date_reviewed = now()
+
+        super(Book, self).save(*args, **kwargs)
+
 
 class Author(models.Model):
     name = models.CharField(max_length=70, help_text="Use pen name, not real name", unique=True)
